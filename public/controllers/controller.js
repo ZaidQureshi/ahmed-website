edf92eccd3e52f0ec6c4a39a20aff347740437da
Marufur Bhuiya <mb4833@nyu.edu>
2016-09-19 16:00:42 -0400

    Fixed formatting issues and added store section

---
+++ public/controllers/controller.js
@@ -0,0 +1,90 @@
+/*
+Controller to interact with views(what the user sees)
+and models(the data, logic, information)
+
+- Allow the view(store.html) to access information regarding templates and display them
+- Recieve user input from the view and send it to the model through the server
+*/
+
+
+// [] contains the dependencies
+var myApp = angular.module('myApp', []); 
+
+
+myApp.controller('AppControl', ['$scope', '$http',
+	function($scope, $http){
+		console.log("Hello World from controller");
+		
+		// Send a request to get data from the database
+		$scope.refresh = function(){
+			// Request data from the server
+			$http.get('/templates').success(function(response){
+				console.log("I got the data I requested");
+				
+				// $scope to use the templates in the html
+				$scope.templates = response;
+			
+			});
+		}
+		
+		$scope.view = function(){
+			// To be completed
+			});
+		}
+		$scope.addCart = function(){
+			// To be completed
+			});
+		}
+		
+		
+		// Generate the intial data on page load
+		$scope.refresh();
+			
+			
+			
+		/*refresh();
+		
+		$scope.templateInfo = function(id){
+			console.log(id);	
+			$http.get('/templates/' + id).success(function(response){
+				console.log("I got the data I requested");
+				//window.location.href = "http://localhost:3000/menu.html";
+				$scope.store = response;
+				console.log(window.location.href);
+			});
+		};
+		
+		// Send data to POST request from server to add to the database
+		$scope.addTemplate = function() {
+				//console.log($scope.template);
+				$http.post('/templates', $scope.template).success(function(response){
+					console.log(response);
+					refresh();
+				});
+				
+		};
+		
+		$scope.remove = function(id){
+			console.log(id);
+			$http.delete('/templates/' + id).success(function(response){
+				refresh();
+			});
+		};
+		
+		
+		$scope.edit = function(id) {
+			console.log(id);
+			$http.get('/templates/' + id).success(function(response){
+				$scope.template = response;
+			});
+		};
+		
+		$scope.update = function() {
+			console.log($scope.template._id);
+			$http.put('/templates/' + $scope.template._id, $scope.template).success(function(response){
+				refresh();
+			});
+		};
+		*/
+
+	}]);
--- server.js
+++ server.js
@@ -9,10 +9,30 @@ var express = require('express');
 // Require ejs modules to be used for live binding to html page
 var ejs = require('ejs');
 
+// Require mongojs modules to interact with database
+var mongojs = require('mongojs');
+var db = mongojs('templates', ['templates']); 
 
 var app = express();
 // Allow public files (html, css, javascript) to be run on the server
 app.use(express.static(__dirname + "/public"));	
+
+
+
+
+// Sends back to the controller the GET data requested
+app.get('/templates', function(req, res){
+	console.log("I received a GET request");
+	
+	// Get the data from the database
+	db.templates.find(function (err, docs){
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+
+
 
 app.listen(3000);
 console.log("Server running on port 3000");
